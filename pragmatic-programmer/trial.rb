b6ac5f9b06bcdbd4eba849e7617bab76e363a7be
JoelTow1990 <joeltowell@hotmail.com>
2024-05-01 23:39:46 +1000

    tidy

--- pragmatic-programmer/trial.rb
+++ pragmatic-programmer/trial.rb
@@ -1,6 +1,6 @@
-big_var = 1000
-small_var = 10
-on_next_call = ->(i) { i ** 2 }
-if big_var > small_var
-  do_something
+bigVar = 1000
+smallVar = 10
+onNextCall = ->(i) { i ** 2 }
+if bigVar > smallVar
+  doSomething
 end
--- pragmatic-programmer/trial0.rb
+++ pragmatic-programmer/trial0.rb
@@ -1,5 +1,5 @@
-this_array = [1,2,3]
-this_array.map! { |my_var| my_var + 1 }
+thisArray = [1,2,3]
+thisArray.map! { |myVar| myVar + 1 }
 if true
   puts "hello"
 end
